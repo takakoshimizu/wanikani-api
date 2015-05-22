4683dc51bb3a6b793fc6f56b290254617740c96d
Erich Dalton <erich.t.dalton.ctr@mail.mil>
2015-05-22 13:43:12 -0400

    Removed reundant wrapper class. Instead of only exposing public methods, will just refactor to prefix private methods and variables with underscores. Cuts down on code size as well.

--- dist/wkapi.js
+++ dist/wkapi.js
@@ -64,189 +64,160 @@ exports.convertCase = function (obj) {
 },{}],4:[function(require,module,exports){
 /// <reference path="./typings/promise.d.ts" />
 var fetcher_1 = require('./fetcher');
-var WkCache = (function () {
-    function WkCache(apiKey) {
+var WkApi = (function () {
+    function WkApi(_apiKey) {
+        this._apiKey = _apiKey;
         this._expiryTime = 3600;
         this._userInformation = {};
         this._studyQueue = {};
         this._levelProgress = {};
         this._srsDistribution = {};
         this._recentUnlocks = {};
-        this.storageKeys = ['_userInformation', '_studyQueue', '_levelProgress',
+        this._storageKeys = ['_userInformation', '_studyQueue', '_levelProgress',
             '_srsDistribution', '_recentUnlocks'];
-        this._fetcher = new fetcher_1.Fetcher(apiKey);
+        if (_apiKey.length !== 32 || !_apiKey.match(/[A-z0-9]{32}/)) {
+            throw 'Invalid API Key. API Key must be 32 alphanumeric characters in length.';
+        }
+        this._fetcher = new fetcher_1.Fetcher(_apiKey);
         if (window.localStorage) {
-            this.loadLocalStorage();
+            this._loadLocalStorage();
         }
     }
-    WkCache.prototype.getUserInformation = function () {
+    WkApi.prototype.getUserInformation = function () {
         var _this = this;
-        if (this.isValid(this._userInformation)) {
+        if (this._isValid(this._userInformation)) {
             return Promise.resolve(this._userInformation.data);
         }
         return new Promise(function (resolve, reject) {
             var data = _this._fetcher.getData('user-information');
             data.then(function (value) {
-                _this.setCacheItem(_this._userInformation, value);
+                _this._setCacheItem(_this._userInformation, value);
                 resolve(value.userInformation);
             }).catch(function () {
                 reject();
             });
         });
     };
-    WkCache.prototype.getStudyQueue = function () {
+    WkApi.prototype.getStudyQueue = function () {
         var _this = this;
-        if (this.isValid(this._studyQueue)) {
+        if (this._isValid(this._studyQueue)) {
             return Promise.resolve(this._studyQueue.data);
         }
         return new Promise(function (resolve, reject) {
             var data = _this._fetcher.getData('study-queue');
             data.then(function (value) {
-                _this.setCacheItem(_this._studyQueue, value);
+                _this._setCacheItem(_this._studyQueue, value);
                 resolve(value.requestedInformation);
             }).catch(function () {
                 reject();
             });
         });
     };
-    WkCache.prototype.getLevelProgression = function () {
+    WkApi.prototype.getLevelProgression = function () {
         var _this = this;
-        if (this.isValid(this._levelProgress)) {
+        if (this._isValid(this._levelProgress)) {
             return Promise.resolve(this._levelProgress.data);
         }
         return new Promise(function (resolve, reject) {
             var data = _this._fetcher.getData('level-progression');
             data.then(function (value) {
-                _this.setCacheItem(_this._levelProgress, value);
+                _this._setCacheItem(_this._levelProgress, value);
                 resolve(value.requestedInformation);
             }).catch(function () {
                 reject();
             });
         });
     };
-    WkCache.prototype.getSrsDistribution = function () {
+    WkApi.prototype.getSrsDistribution = function () {
         var _this = this;
-        if (this.isValid(this._srsDistribution)) {
+        if (this._isValid(this._srsDistribution)) {
             return Promise.resolve(this._srsDistribution.data);
         }
         return new Promise(function (resolve, reject) {
             var data = _this._fetcher.getData('srs-distribution');
             data.then(function (value) {
-                _this.setCacheItem(_this._srsDistribution, value);
+                _this._setCacheItem(_this._srsDistribution, value);
                 resolve(value.requestedInformation);
             }).catch(function () {
                 reject();
             });
         });
     };
-    WkCache.prototype.getRecentUnlocks = function (count) {
+    WkApi.prototype.getRecentUnlocks = function (count) {
         var _this = this;
         if (count === void 0) { count = 10; }
         var overrideCache = false;
         if (this._recentUnlocks.data) {
             overrideCache = this._recentUnlocks.data.length != count;
         }
-        if (this.isValid(this._recentUnlocks) && !overrideCache) {
+        if (this._isValid(this._recentUnlocks) && !overrideCache) {
             return Promise.resolve(this._recentUnlocks.data);
         }
         return new Promise(function (resolve, reject) {
             var data = _this._fetcher.getData('recent-unlocks', count);
             data.then(function (value) {
-                _this.setCacheItem(_this._recentUnlocks, value);
+                _this._setCacheItem(_this._recentUnlocks, value);
                 resolve(value.requestedInformation);
             }).catch(function () {
                 reject();
             });
         });
     };
-    WkCache.prototype.setExpiry = function (time) {
+    WkApi.prototype.setExpiry = function (time) {
         this._expiryTime = time;
     };
-    WkCache.prototype.setCacheItem = function (cacheItem, apiItem) {
+    WkApi.prototype._setCacheItem = function (cacheItem, apiItem) {
         if (apiItem.requestedInformation) {
             cacheItem.data = apiItem.requestedInformation;
-            cacheItem.lastUpdated = this.getTime();
+            cacheItem.lastUpdated = this._getTime();
         }
         if (apiItem.userInformation) {
             this._userInformation = {
                 data: apiItem.userInformation,
-                lastUpdated: this.getTime()
+                lastUpdated: this._getTime()
             };
         }
-        this.persistLocalStorage();
+        this._persistLocalStorage();
     };
-    WkCache.prototype.loadLocalStorage = function () {
-        for (var _i = 0, _a = this.storageKeys; _i < _a.length; _i++) {
+    WkApi.prototype._loadLocalStorage = function () {
+        for (var _i = 0, _a = this._storageKeys; _i < _a.length; _i++) {
             var key = _a[_i];
             if (window.localStorage.getItem(key)) {
                 this[key] = JSON.parse(window.localStorage.getItem(key));
             }
         }
     };
-    WkCache.prototype.persistLocalStorage = function () {
-        for (var _i = 0, _a = this.storageKeys; _i < _a.length; _i++) {
+    WkApi.prototype._persistLocalStorage = function () {
+        for (var _i = 0, _a = this._storageKeys; _i < _a.length; _i++) {
             var key = _a[_i];
             if (this[key]) {
                 window.localStorage.setItem(key, JSON.stringify(this[key]));
             }
         }
     };
-    WkCache.prototype.clearLocalStorage = function () {
+    WkApi.prototype._clearLocalStorage = function () {
         window.localStorage.clear();
     };
-    WkCache.prototype.isValid = function (cacheItem) {
+    WkApi.prototype._isValid = function (cacheItem) {
         if (!cacheItem)
             return false;
         if (!cacheItem.lastUpdated)
             return false;
-        var now = this.getTime();
+        var now = this._getTime();
         var maxValidity = cacheItem.lastUpdated + this._expiryTime;
         return maxValidity > now;
     };
-    WkCache.prototype.getTime = function () {
+    WkApi.prototype._getTime = function () {
         return (new Date()).getTime() / 1000;
-    };
-    return WkCache;
-})();
-exports.WkCache = WkCache;
-
-},{"./fetcher":1}],5:[function(require,module,exports){
-var wkCache_1 = require('./wkCache');
-var WkApi = (function () {
-    function WkApi(_apiKey) {
-        this._apiKey = _apiKey;
-        if (_apiKey.length !== 32 || !_apiKey.match(/[A-z0-9]{32}/)) {
-            throw 'Invalid API Key. API Key must be 32 alphanumeric characters in length.';
-        }
-        this._cache = new wkCache_1.WkCache(_apiKey);
-    }
-    WkApi.prototype.setExpiry = function (time) {
-        this._cache.setExpiry(time);
-    };
-    WkApi.prototype.getUserInformation = function () {
-        return this._cache.getUserInformation();
-    };
-    WkApi.prototype.getStudyQueue = function () {
-        return this._cache.getStudyQueue();
-    };
-    WkApi.prototype.getLevelProgression = function () {
-        return this._cache.getLevelProgression();
-    };
-    WkApi.prototype.getSrsDistribution = function () {
-        return this._cache.getSrsDistribution();
-    };
-    WkApi.prototype.getRecentUnlocks = function (count) {
-        if (count === void 0) { count = 10; }
-        return this._cache.getRecentUnlocks(count);
     };
     return WkApi;
 })();
 exports.WkApi = WkApi;
 
-},{"./wkCache":4}],6:[function(require,module,exports){
+},{"./fetcher":1}],5:[function(require,module,exports){
 (function (global){
 var wkapi_1 = require('./wkapi');
 global.WkApi = wkapi_1.WkApi;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./wkapi":5}]},{},[6]);
+},{"./wkapi":4}]},{},[5]);
--- src/wkapi.ts
+++ src/wkapi.ts
@@ -1,47 +1,200 @@
-﻿import {
-    IUserInformation, IStudyQueue, ILevelProgress,
-    ISRSDistribution, IRecentUnlock } from './typings/apiTypes';
-import { IWkCache, WkCache } from './wkCache';
+﻿/// <reference path="./typings/promise.d.ts" />
 
-export class WkApi {
-    private _cache: IWkCache;
+import {
+    IApiResponse, IApiInformation, IUserInformation,
+    IStudyQueue, ILevelProgress, ISRSDistribution,
+    IRecentUnlock } from './typings/apiTypes';
+import { IFetcher, Fetcher } from './fetcher';
 
+interface ICache<T> {
+    lastUpdated: number;
+    data: T;
+}
+
+export interface IWkApi {
+    getUserInformation(): Promise<IUserInformation>;
+    getStudyQueue(): Promise<IStudyQueue>;
+    getLevelProgression(): Promise<ILevelProgress>;
+    getSrsDistribution(): Promise<ISRSDistribution>;
+    getRecentUnlocks(count: number): Promise<IRecentUnlock[]>;
+    setExpiry(time: number): void;
+}
+
+export class WkApi implements IWkApi {
+    private _fetcher: IFetcher;
+    private _expiryTime: number = 3600; // seconds
+    private _userInformation: ICache<IUserInformation> = <ICache<IUserInformation>> {};
+    private _studyQueue: ICache<IStudyQueue> = <ICache<IStudyQueue>> {};
+    private _levelProgress: ICache<ILevelProgress> = <ICache<ILevelProgress>> {};
+    private _srsDistribution: ICache<ISRSDistribution> = <ICache<ISRSDistribution>> {};
+    private _recentUnlocks: ICache<IRecentUnlock[]> = <ICache<IRecentUnlock[]>> {};
+
+    private _storageKeys = ['_userInformation', '_studyQueue', '_levelProgress',
+                            '_srsDistribution', '_recentUnlocks'];
+    
     constructor(private _apiKey: string) {
         // validate apiKey format
         if (_apiKey.length !== 32 || !_apiKey.match(/[A-z0-9]{32}/)) {
             throw 'Invalid API Key. API Key must be 32 alphanumeric characters in length.';
         }
 
-        this._cache = new WkCache(_apiKey);
+        this._fetcher = new Fetcher(_apiKey);
+
+        // load from local storage, if available
+        if (window.localStorage) {
+            this._loadLocalStorage();
+        }
     }
     
-    // Sets the cache expiry time in seconds
-    public setExpiry(time: number): void {
-        this._cache.setExpiry(time);
-    }
-    
-    // Returns the User Information segment cached off
-    // the latest request, wrapped in a Promise
+    // Returns the cached user information in a promise if still valid.
+    // Otherwise, returns a promise containing incoming information
     public getUserInformation(): Promise<IUserInformation> {
-        return this._cache.getUserInformation();
+        if (this._isValid(this._userInformation)) {
+            return Promise.resolve<IUserInformation>(this._userInformation.data);
+        }
+        return new Promise<IUserInformation>((resolve, reject) => {
+            let data = this._fetcher.getData<IApiResponse<IUserInformation>>('user-information');
+            data.then((value: IApiResponse<IUserInformation>) => {
+                this._setCacheItem(this._userInformation, value);
+                resolve(value.userInformation);
+            }).catch(() => {
+                reject();
+            });
+        });
     }
 
-    // Returns the current user's study queue
+    // Returns the cached study queue in a promise if still valid.
+    // Otherwise returns a promise containing incoming information.
     public getStudyQueue(): Promise<IStudyQueue> {
-        return this._cache.getStudyQueue();
+        if (this._isValid(this._studyQueue)) {
+            return Promise.resolve<IStudyQueue>(this._studyQueue.data);
+        }
+        return new Promise<IStudyQueue>((resolve, reject) => {
+            let data = this._fetcher.getData<IApiResponse<IStudyQueue>>('study-queue');
+            data.then((value: IApiResponse<IStudyQueue>) => {
+                this._setCacheItem(this._studyQueue, value);
+                resolve(value.requestedInformation);
+            }).catch(() => {
+                reject();
+            });
+        });
     }
 
-    // Returns the current user's level progression
+    // Returns the cached level progression in a promise if still valid.
+    // Otherwise returns a promise containing the incoming information.
     public getLevelProgression(): Promise<ILevelProgress> {
-        return this._cache.getLevelProgression();
+        if (this._isValid(this._levelProgress)) {
+            return Promise.resolve<ILevelProgress>(this._levelProgress.data);
+        }
+        return new Promise<ILevelProgress>((resolve, reject) => {
+            let data = this._fetcher.getData<IApiResponse<ILevelProgress>>('level-progression');
+            data.then((value: IApiResponse<ILevelProgress>) => {
+                this._setCacheItem(this._levelProgress, value);
+                resolve(value.requestedInformation);
+            }).catch(() => {
+                reject();
+            });
+        });
     }
 
-    // returns the current user's SRS distribution
+    // Returns the cached SRS Distribution in a promise if still valid.
+    // Otherwise returns a promise containing the incoming information.
     public getSrsDistribution(): Promise<ISRSDistribution> {
-        return this._cache.getSrsDistribution();
+        if (this._isValid(this._srsDistribution)) {
+            return Promise.resolve<ISRSDistribution>(this._srsDistribution.data);
+        }
+        return new Promise<ISRSDistribution>((resolve, reject) => {
+            let data = this._fetcher.getData<IApiResponse<ISRSDistribution>>('srs-distribution');
+            data.then((value: IApiResponse<ISRSDistribution>) => {
+                this._setCacheItem(this._srsDistribution, value);
+                resolve(value.requestedInformation);
+            }).catch(() => {
+                reject();
+            });
+        });
     }
 
+    // Returns the recent unlocks list in a promise if still valid.
+    // Otherwise returns a promise containing the incoming information
+    // Takes a count between 1 and 100. Will Override the cache if the number
+    // has changed
     public getRecentUnlocks(count: number = 10): Promise<IRecentUnlock[]> {
-        return this._cache.getRecentUnlocks(count);
+        let overrideCache = false;
+        if (this._recentUnlocks.data) {
+            overrideCache = this._recentUnlocks.data.length != count;
+        }
+
+        if (this._isValid(this._recentUnlocks) && !overrideCache) {
+            return Promise.resolve<IRecentUnlock[]>(this._recentUnlocks.data);
+        }
+        return new Promise<IRecentUnlock[]>((resolve, reject) => {
+            let data = this._fetcher.getData<IApiResponse<IRecentUnlock[]>>('recent-unlocks', count);
+            data.then((value: IApiResponse<IRecentUnlock[]>) => {
+                this._setCacheItem(this._recentUnlocks, value);
+                resolve(value.requestedInformation);
+            }).catch(() => {
+                reject();
+            });
+        });
+    }
+
+    // Sets the expiry time in seconds
+    public setExpiry(time: number): void {
+        this._expiryTime = time;
+    }
+
+    private _setCacheItem(cacheItem: ICache<{}>, apiItem: IApiResponse<{}>) {
+        if (apiItem.requestedInformation) {
+            cacheItem.data = apiItem.requestedInformation;
+            cacheItem.lastUpdated = this._getTime();
+        }
+
+        // always update user information from every request
+        if (apiItem.userInformation) {
+            this._userInformation = {
+                data: apiItem.userInformation,
+                lastUpdated: this._getTime()
+            };
+        }
+
+        this._persistLocalStorage();
+    }
+
+    // Loads all persisted information from localStorage
+    private _loadLocalStorage() {
+        for (let key of this._storageKeys) {
+            if (window.localStorage.getItem(key)) {
+                (<any>this)[key] = JSON.parse(window.localStorage.getItem(key));
+            }
+        }
+    }
+
+    // Persists all information to localStorage
+    private _persistLocalStorage() {
+        for (let key of this._storageKeys) {
+            if ((<any>this)[key]) {
+                window.localStorage.setItem(key, JSON.stringify((<any>this)[key]));
+            }
+        }
+    }
+
+    // Clears all used localStorage
+    private _clearLocalStorage() {
+        window.localStorage.clear();
+    }
+    
+    // Returns if the supplied cache item is still valid
+    // based on the current expiry time
+    private _isValid(cacheItem: ICache<{}>): boolean {
+        if (!cacheItem) return false;
+        if (!cacheItem.lastUpdated) return false;
+        let now = this._getTime();
+        let maxValidity = cacheItem.lastUpdated + this._expiryTime;
+        return maxValidity > now;
+    }
+    
+    // Returns the current unix epoch time
+    private _getTime(): number {
+        return (new Date()).getTime() / 1000;
     }
 }
